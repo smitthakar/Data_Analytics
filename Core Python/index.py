65cd799cb2dd06e5eb00480a089dbc5ed788f955
JAYDEEP BHADORIYA-DA <124075329+jaybhadoriya@users.noreply.github.com>
2026-06-27 21:03:25 +0530

    Performing python task

--- Core Python/index.py
+++ Core Python/index.py
@@ -1,1 +1,15 @@
-print('hello')
+# print('hello')
+# print('smit')
+
+# Name = input("What's your name: ")
+# age =int(input("what's your age: "))
+
+# print("My name is", Name , "and my is ", age )
+
+Name = input("Enter name: ")
+Course = input("Enter your course: ")
+Fees = int(input("Course fees: "))
+
+
+print("my name is",Name, "and i choose ", Course, "course and it's fees ",Fees)
+
