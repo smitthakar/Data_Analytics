c7684d544b2808e8899b310caddd8c673b667543
Smit Thakar <159602019+smitthakar@users.noreply.github.com>
2026-06-28 10:20:10 +0530

    Add files via upload

--- Core Python/index.py
+++ Core Python/index.py
@@ -1,15 +1,58 @@
 # print('hello')
 # print('smit')
 
+# Q2. User Input
 # Name = input("What's your name: ")
 # age =int(input("what's your age: "))
 
 # print("My name is", Name , "and my is ", age )
 
-Name = input("Enter name: ")
-Course = input("Enter your course: ")
-Fees = int(input("Course fees: "))
+
+# Q3. Multiple Inputs
+
+# Name = input("Enter name: ")
+# Course = input("Enter your course: ")
+# Fees = int(input("Course fees: "))
 
 
-print("my name is",Name, "and i choose ", Course, "course and it's fees ",Fees)
+# print("my name is",Name, "and i choose ", Course, "course and it's fees ",Fees)
+
+# Section 2: Variables Practice
+
+# Q4. Variable Assignment
+
+# Std_Name = input("Enter your name : " )
+# Marks = int(input("Enter marks : "))
+
+# if Marks >= 35 :
+#     print("Passed")
+# else:
+#     print("Failed")
+
+
+# Q6. Swap Two Variables
+
+# a= 10
+# b= 20
+# a,b =b,a
+
+# print("a:",a)
+# print("b:",b)
+
+# Q7. Identify Data Types
+# num= 12
+# salary = 2000.12
+# name = "Smit"
+# boolen = False
+
+# print(type(num))
+# print(type(salary))
+# print(type(name))
+# print(type(boolen))
+
+age = int(input("Enter age:" ))
+salary = float(input("Enter salary: "))
+
+print(type(age))
+print(type(salary))
 
